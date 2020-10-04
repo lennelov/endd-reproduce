1a793227af856743f0967b751bfbf69830eb7492
lennelov <lennelov@kth.se>
2020-10-04 19:27:18 +0200

    Update

--- code/models/vgg.py
+++ code/models/vgg.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import tensorflow.keras as keras
-import models.model_globals as model_globals
+import settings
 
 
 def get_model(dataset_name, compile=True):
@@ -8,7 +8,7 @@ def get_model(dataset_name, compile=True):
 
     Args:
         dataset_name (str): Name of the dataset that the model will be used on,
-                            must be listed in model_globals.py.
+                            must be listed in settings.py.
         compile (bool): If False, an uncompiled model is returned. Default is True.
 
     Returns:
@@ -17,14 +17,17 @@ def get_model(dataset_name, compile=True):
     If compile=True, model will be compiled with adam optimizer, categorical cross
     entropy loss, and accuracy metric.
     """
-    if dataset_name not in model_globals.DATASET_INPUT_SHAPES:
-        raise ValueError("Dataset {} not recognized".format(dataset_name))
+    if dataset_name not in settings.DATASET_NAMES:
+        raise ValueError("""Dataset {} not recognized, please make sure it has been listed in
+                            settings.py""".format(dataset_name))
+
 
     model = tf.keras.applications.VGG16(
         include_top=True,
         weights=None,
-        input_shape=model_globals.DATASET_INPUT_SHAPES[dataset_name],
-        classes=model_globals.DATASET_N_CLASSES[dataset_name])
+        input_shape=settings.DATASET_INPUT_SHAPES[dataset_name],
+        classes=settings.DATASET_N_CLASSES[dataset_name]
+    )
 
     if not compile:
         return model
