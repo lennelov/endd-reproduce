13119c43bc4ab3f4fa8998d39ffdb70c72725b33
lennelov <lennelov@kth.se>
2020-10-04 19:39:09 +0200

    Update

--- code/scripts/train_cnn.py
+++ code/scripts/train_cnn.py
@@ -7,15 +7,15 @@ training script."""
 import sys
 import pathlib
 import os
-parent_dir_path = str(pathlib.Path(__file__).parent.parent.absolute())
+parent_dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(parent_dir_path)
 
 # Imports
 import models.cnn as cnn
+import utils.saveload as saveload
 import tensorflow as tf
 import tensorflow.keras as keras
 import category_encoders as ce
-import pandas as pd
 from tensorflow.keras import datasets
 
 # Need these settings for GPU to work on my computer /Einar
@@ -23,15 +23,17 @@ physical_devices = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 # Load data
-(train_images, train_labels), (test_images,
-                               test_labels) = datasets.cifar10.load_data()
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 
 # Preprocess
-train_labels = tf.one_hot(train_labels.reshape((-1, )), 10)
-test_labels = tf.one_hot(test_labels.reshape((-1, )), 10)
+train_labels = tf.one_hot(train_labels.reshape((-1,)), 10)
+test_labels = tf.one_hot(test_labels.reshape((-1,)), 10)
 
 # Get model
 model = cnn.get_model(dataset_name='cifar10', compile=True)
 
 # Train
-model.fit(train_images, train_labels, epochs=200, validation_data=(test_images, test_labels))
+model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
+
+# Save weights
+saveload.save_weights(model, "cnn")
