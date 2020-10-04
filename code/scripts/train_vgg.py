1358a9998551cb1f69a894f8619f455921771449
lennelov <39123876+lennelov@users.noreply.github.com>
2020-10-04 21:54:33 +0200

    Save and load (#12)
    Adds some saving and loading functionality, and adds this to model and training code

--- code/scripts/train_vgg.py
+++ code/scripts/train_vgg.py
@@ -7,15 +7,15 @@ training script."""
 import sys
 import pathlib
 import os
-parent_dir_path = str(pathlib.Path(__file__).parent.parent.absolute())
+parent_dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(parent_dir_path)
 
 # Imports
 import models.vgg as vgg
+import utils.saveload as saveload
 import tensorflow as tf
 import tensorflow.keras as keras
 import category_encoders as ce
-import pandas as pd
 from tensorflow.keras import datasets
 from tensorflow.keras.applications.vgg16 import preprocess_input
 
@@ -24,17 +24,19 @@ physical_devices = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 # Load data
-(train_images, train_labels), (test_images,
-                               test_labels) = datasets.cifar10.load_data()
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 
 # Preprocess
 train_images = preprocess_input(train_images)
 test_images = preprocess_input(test_images)
-train_labels = tf.one_hot(train_labels.reshape((-1, )), 10)
-test_labels = tf.one_hot(test_labels.reshape((-1, )), 10)
+train_labels = tf.one_hot(train_labels.reshape((-1,)), 10)
+test_labels = tf.one_hot(test_labels.reshape((-1,)), 10)
 
 # Get model
 model = vgg.get_model(dataset_name='cifar10', compile=True)
 
 # Train
-model.fit(train_images, train_labels, epochs=100)
+model.fit(train_images, train_labels, epochs=1, validation_data=(test_images, test_labels))
+
+# Save weights
+saveload.save_weights(model, "vgg")
