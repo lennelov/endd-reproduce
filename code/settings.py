7abb45e1035c42221b508fb7c73e9c3b49ba3dff
lennelov <lennelov@kth.se>
2020-10-04 19:34:11 +0200

    Add

---
+++ code/settings.py
@@ -0,0 +1,17 @@
+# Paths
+SAVED_WEIGHTS_PATH = "code/models/saved_weights"
+SAVED_MODELS_PATH = "code/models/saved_models"
+
+# Dataset metadata
+DATASET_NAMES = [
+    "cifar10",
+    "mnist"
+]
+DATASET_INPUT_SHAPES = {
+    "cifar10": (32, 32, 3),
+    "mnist": (32, 32, 1)
+}
+DATASET_N_CLASSES = {
+    "cifar10": 10,
+    "mnist": 10
+}
