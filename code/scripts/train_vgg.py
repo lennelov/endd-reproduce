706f039b0322aa27890259b652673c6da10e1894
lennelov <lennelov@kth.se>
2020-10-04 13:49:53 +0200

    Add train VGG script

--- code/scripts/train_vgg.py
+++ code/scripts/train_vgg.py
@@ -1,9 +1,20 @@
+# Add parent dir to path to allow for parallel imports
+import sys
+import pathlib
+import os
+parent_dir_path = str(pathlib.Path(__file__).parent.parent.absolute())
+sys.path.append(parent_dir_path)
+
+# Imports
+import models.vgg as vgg
 import tensorflow as tf
+import tensorflow.keras as keras
 import category_encoders as ce
 import pandas as pd
 from tensorflow.keras import datasets
 from tensorflow.keras.applications.vgg16 import preprocess_input
 
+# Need these settings for GPU to work on my computer /Einar
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
@@ -12,34 +23,14 @@ tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 # Preprocess
 train_images = preprocess_input(train_images)
-train_labels = train_labels.reshape((-1, ))
-train_labels = tf.one_hot(train_labels, 10)
+test_images = preprocess_input(test_images)
+train_labels = tf.one_hot(train_labels.reshape((-1,)), 10)
+test_labels = tf.one_hot(test_labels.reshape((-1,)), 10)
 
-# Build model
-model = tf.keras.applications.VGG16(include_top=True,
-                                    weights=None,
-                                    input_shape=(32, 32, 3),
-                                    classes=10)
-
-
-preds = model.predict(train_images)
-
-# preds.shape
-# train_labels.shape
-# preds[:5, :]
-# train_labels[:5, :]
-
-model.compile(optimizer='adam',
-              loss='categorical_crossentropy',
-              metrics=[
-              tf.keras.metrics.Accuracy(name='accuracy')
-              ])
+# Get model
+model = vgg.get_model(dataset_name='cifar10', compile=True)
 
 # Train
-model.fit(train_images, train_labels, epochs=1)
+model.fit(train_images, train_labels, epochs=10)
 
 model.summary()
-
-
-# print(preds[:5, :])
-# print(train_labels[:5, :])
