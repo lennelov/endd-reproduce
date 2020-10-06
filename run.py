16bfe2bb61e65fb3ebc9937fc51fff15f316972a
Aron Noren <47273486+AronNoren@users.noreply.github.com>
2020-10-06 08:30:55 +0200

    Update run.py
    updated to include simplex plots and be compatible with settings_prior.py

--- run.py
+++ run.py
@@ -1,8 +1,14 @@
+# Add parent dir to path to allow for parallel imports
+import sys
+import pathlib
+import os
+parent_dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(parent_dir_path)
+
 #packages
 import tensorflow as tf
 from tensorflow.keras import layers, models
-from tensorflow.math import lgamma
-from tensorflow.math import digamma
+from tensorflow.math import lgamma,digamma
 import numpy as np
 import torch # only used for KaosEngineers dataset
 from torch.utils.data import Dataset, DataLoader # only used for KaosEngineers dataset
@@ -13,9 +19,9 @@ import seaborn as sns
 import keras
 from datetime import datetime
 from packaging import version
-
 #other functions
-
+import settings
+import simplex_plot_function
 from preprocess_toy_dataset import preprocess_toy_dataset
 from DirichletKL import DirichletKL
 from create_toy_dataset_model import create_toy_dataset_model
@@ -23,17 +29,32 @@ from train_priornet_toy_dataset import train_priornet_toy_dataset
 from dataset_creation_KaosEngineer import SpiralDataset
 from dataset_creation_KaosEngineer import OODSpiralDataset
 
-def main():
-      n_classes = 3
-      Spiral = SpiralDataset(10000,0.2,n_classes)
-      OOD = OODSpiralDataset(10000)
-      x_train,y_train,x_test,y_test = preprocess_toy_dataset(Spiral,OOD)
-      model = create_toy_dataset_model(n_classes,n_layers = 5, n_neurons = 64,activations = 'relu')
-      KL = DirichletKL()
-      model.compile(optimizer='adam',loss = KL)
-      model = train_priornet_toy_dataset(x_train,y_train,model)
-      logits = model.predict(x_test)
-      predictions = tf.math.argmax(logits,axis = 1)
-      real = tf.math.argmax(y_test,axis = 1)
-      print(tf.reduce_sum(tf.cast(predictions ==real, tf.float32))/len(real))
-main()
+Spiral = SpiralDataset(SAMPLES_PER_CLASS,NOISE,N_CLASSES)
+OOD = OODSpiralDataset(SAMPLES_OOD)
+x_train,y_train,x_test,y_test = preprocess_toy_dataset(Spiral,OOD)
+model = create_toy_dataset_model(n_classes,N_LAYERS,N_NEURONS,activations = ACTIVATION)
+
+KL = DirichletKL()
+model.compile(optimizer='adam',loss = KL)
+model = train_priornet_toy_dataset(x_train,y_train,model,BATCH_SIZE,N_EPOCHS)
+logits = model.predict(x_test)
+predictions = tf.math.argmax(logits,axis = 1)
+real = tf.math.argmax(y_test,axis = 1)
+if PLOT_SIMPLEX:
+    import seaborn as sn
+    font = {'family': 'serif',
+            'color':  'black',
+            'weight': 'normal',
+            'size': 16,
+            }
+    plt.style.use('seaborn-white')
+    plt.figure(num=None, figsize=(16, 12), dpi=80, facecolor='w', edgecolor='k')
+
+    for i in range(0,6):
+        plt.subplot(2, 3, i+1)
+        plt.title("logits: " + str(np.around(logits[i,:],decimals =1)) ,
+                fontsize=18, ha='center')
+        plot_logits = logits[i,:]
+        draw_pdf_contours(Dirichlet(plot_logits))
+
+      
