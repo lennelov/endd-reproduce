2e0433150992923635b6c79dce3ce580049fa574
lennelov <lennelov@kth.se>
2020-10-08 21:08:21 +0200

    Update train_ensemble

--- code/scripts/train_ensemble.py
+++ code/scripts/train_ensemble.py
@@ -1,13 +1,31 @@
-"""Script for training a simple CNN model on CIFAR10.
+"""Script for training an ensemble of models.
 
-This script will probably be generalized to a unified
-training script."""
+Instructions for training:
+    1. Set global parameters below.
+    2. Run script from repository root.
+    3. Saved models are now found in SAVED_MODELS_PATH with names
+       {ENSEMBLE_SAVE_NAME}_{DATASET_NAME}_{model_number}, and are
+       also listed in SAVED_MODELS_PATH/ensemble_model_names.json
 
-MODEL_TYPE = 'cnn'
-MODEL_SAVE_NAME = 'basic_cnn'
-DATASET_NAME = 'cifar10'
-N_MODELS = 10
-N_EPOCHS = 15
+Instructions for building ensemble with trained models (see also ensemble_example_usage_2.py):
+    1. Fetch record of all ensembles with
+           ensemble_model_names = utils.saveload.get_ensemble_model_names()
+    2. Get relevant model_names with
+           model_names = ensemble_model_names[ENSEMBLE_SAVE_NAME][DATASET_NAME]
+    3. Wrap models with
+           models = [models.ensemble.KerasLoadsWhole(name) for name in model_names]
+    4. Build ensemble of models with
+           ensemble = models.ensemble.Ensemble(models)
+    5. Make ensemble prediction with
+           ensemble.predict(x)
+"""
+
+MODEL_TYPE = 'cnn'  # Name of model module {cnn, vgg}  Note: probably won't work with vgg
+ENSEMBLE_SAVE_NAME = 'basic_cnn'  # Name that the ensemble models will be saved with
+NAME_START_NUMBER = 0  # Start number for model naming (set to 0 unless continuing previous training)
+DATASET_NAME = 'cifar10'  # Name of dataset {cifar10, cifar100, mnist}
+N_MODELS = 20  # Number of models to train
+N_EPOCHS = 15  # Number of epochs to train for
 
 # Add parent dir to path to allow for parallel imports
 import sys
@@ -30,21 +48,34 @@ tf.config.experimental.set_memory_growth(physical_devices[0], True)
 (train_images, train_labels), (test_images, test_labels) = datasets.get_dataset(DATASET_NAME)
 
 # Preprocess
-train_labels = tf.one_hot(train_labels.reshape((-1,)), 10)
-test_labels = tf.one_hot(test_labels.reshape((-1,)), 10)
+train_labels = tf.one_hot(train_labels.reshape((-1,)), settings.DATASET_N_CLASSES[DATASET_NAME])
+test_labels = tf.one_hot(test_labels.reshape((-1,)), settings.DATASET_N_CLASSES[DATASET_NAME])
 
 # Get model module (python file with get_model function)
 model_module = settings.MODEL_MODULES[MODEL_TYPE]
 
-# Get model
-for i in range(N_MODELS):
-    print("Training model {}...".format(i))
-    # Get model
-    model = model_module.get_model(dataset_name=DATASET_NAME, compile=True)
+# Train and save ensemble
+# Try-finally construct ensures that the list of trained models in the ensemble is
+# update correctly even if script fails before all models have been trained and saved.
+try:
+    saved_model_names = []
+    for i in range(NAME_START_NUMBER, N_MODELS+NAME_START_NUMBER):
+        print("Training model {}...".format(i))
+        # Get model
+        model = model_module.get_model(dataset_name=DATASET_NAME, compile=True)
 
-    # Train model
-    model.fit(train_images, train_labels, epochs=N_EPOCHS, validation_data=(test_images, test_labels))
-    print("Model {} finished training.")
-    # Save model
-    model_name = "{}_{}_{}".format(MODEL_SAVE_NAME, DATASET_NAME, i)
-    saveload.save_tf_model(model, model_name)
+        # Train model
+        model.fit(train_images, train_labels, epochs=N_EPOCHS, validation_data=(test_images, test_labels))
+        print("Model {} finished training.".format(i))
+
+        # Save model
+        model_name = "{}_{}_{}".format(ENSEMBLE_SAVE_NAME, DATASET_NAME, i)
+        saveload.save_tf_model(model, model_name)
+        saved_model_names.append(model_name)
+finally:
+    # Updates ensemble record with list of saved models
+    append_model_names = NAME_START_NUMBER > 0
+    saveload.update_ensemble_names(ENSEMBLE_SAVE_NAME,
+                                   DATASET_NAME,
+                                   saved_model_names,
+                                   append=append_model_names)
