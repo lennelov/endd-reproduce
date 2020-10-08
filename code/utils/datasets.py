a82d04a29f4069e86c3df5fc2e5e08df1ff19ee3
lennelov <39123876+lennelov@users.noreply.github.com>
2020-10-08 21:34:17 +0200

    CNN-ensemble (#20)
    Add script for training CNN ensemble, and some functionality for keeping track of trained models

---
+++ code/utils/datasets.py
@@ -0,0 +1,55 @@
+"""Module collecting datasets for loading.
+
+The _get_dataset functions should return (x_train, y_train), (x_test, y_test)
+where the data is formated as numpy arrays.
+
+Once a function as been added it should also be added to the DATASET_GETTERS list in
+the get_dataset function.
+"""
+import numpy as np
+from tensorflow.keras import datasets
+
+
+def get_dataset(dataset_name):
+    """Take dataset name and return dataset.
+
+    Currently available datasets:
+        'cifar10'
+        'cifar100'
+        'mnist'
+
+    Args:
+        dataset_name (str): Name of dataset.
+
+    Returns:
+        (x_train, y_train), (x_test, y_test) as tuples of numpy arrays.
+    """
+
+    DATASET_GETTERS = {
+        'cifar10': _get_cifar10,
+        'cifar100': _get_cifar100,
+        'mnist': _get_mnist
+    }
+
+    if dataset_name not in DATASET_GETTERS:
+        raise ValueError("""Dataset {} not recognized, please make sure it has been added
+                            to datasets.py""".format(dataset_name))
+
+
+    train_set, test_set = DATASET_GETTERS[dataset_name]()
+    return train_set, test_set
+
+
+def _get_cifar10():
+    return datasets.cifar10.load_data()
+
+
+def _get_cifar100():
+    return datasets.cifar10.load_data()
+
+
+def _get_mnist():
+    (x_train, y_train), (x_test, y_test) = datasets.mnist.load_data()
+    x_train = np.expand_dims(x_train, -1)
+    x_test = np.expand_dims(x_test, -1)
+    return (x_train, y_train), (x_test, y_test)
