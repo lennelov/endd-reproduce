eaae6f279bf47ec49f1c88502800b06d23809133
AronNoren <norenaron@gmail.com>
2020-10-10 13:17:03 +0200

    fixing accidental merge in settings

--- code/settings.py
+++ code/settings.py
@@ -37,11 +37,4 @@ ID_LOGIT = 100
 OOD_LOGIT = 1
 TRAIN_RATIO = 0.7
 
-DATASET_NAMES = ["cifar10", "cifar100", "mnist"]
-DATASET_INPUT_SHAPES = {"cifar10": (32, 32, 3), "cifar100": (32, 32, 3), "mnist": (28, 28, 1)}
-DATASET_N_CLASSES = {"cifar10": 10, "cifar100": 100, "mnist": 10}
-
-# Model metadata
-from models import cnn, vgg
-MODEL_MODULES = {'cnn': cnn, 'vgg': vgg}
 
