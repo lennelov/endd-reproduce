2239a270febe4fc7f46eaaede207deb715cfeac8
lennelov <lennelov@kth.se>
2020-10-11 20:35:47 +0200

    Update

--- code/scripts/evaluation_example_usage.py
+++ code/scripts/evaluation_example_usage.py
@@ -6,6 +6,10 @@ import os
 parent_dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(parent_dir_path)
 
+import tensorflow as tf
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
 from utils import evaluation
 from utils import datasets
 from utils import saveload
