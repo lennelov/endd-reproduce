2e38d1486c8a3c7c141385b11f580bda4ade6c3f
Aron Noren <47273486+AronNoren@users.noreply.github.com>
2020-10-11 20:24:11 +0200

    Pn cifar (#28)
    Adds PriorNet model, as well as script for training it on CIFAR10

--- code/settings.py
+++ code/settings.py
@@ -3,15 +3,33 @@ SAVED_WEIGHTS_PATH = "code/models/saved_weights"
 SAVED_MODELS_PATH = "code/models/saved_models"
 
 # Dataset metadata
-DATASET_NAMES = ["cifar10", "cifar100", "mnist"]
-DATASET_INPUT_SHAPES = {"cifar10": (32, 32, 3),
-                        "cifar100": (32, 32, 3),
-                        "mnist": (28, 28, 1)}
-DATASET_N_CLASSES = {"cifar10": 10, "cifar100": 100, "mnist": 10}
+
+DATASET_NAMES = ["cifar10", "cifar100", "mnist", "synthetic", "cifar10_PN"]
+DATASET_INPUT_SHAPES = {
+    "cifar10": (32, 32, 3),
+    "cifar100": (32, 32, 3),
+    "mnist": (28, 28, 1),
+    "synthetic": (2),
+    "cifar10_PN": (32, 32, 3)
+}
+DATASET_N_CLASSES = {"cifar10": 10, "cifar100": 100, "mnist": 10, "synthetic": 3, "cifar10_PN": 3}
 
 # Model metadata
-from models import cnn, vgg
-MODEL_MODULES = {
-    'cnn': cnn,
-    'vgg': vgg
-}
+from models import cnn, vgg, cnn_priorNet
+MODEL_MODULES = {'cnn': cnn, 'vgg': vgg, 'cnn_priorNet': cnn_priorNet}
+
+# synthetic data hyperparams
+
+SAMPLES_PER_CLASS = 10000
+SAMPLES_OOD = 10000
+NOISE_ID = 0.3
+NOISE_OOD = 0.5
+RADIUS = 410
+ID_NOISE = 0.1
+OOD_NOISE = 0.1
+
+# preprocess data for PN
+
+ID_LOGIT = 100
+OOD_LOGIT = 1
+TRAIN_RATIO = 0.7
