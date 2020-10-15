2a3f275eb4581c6cba4c63b3352e48fe1cd31981
lennelov <lennelov@kth.se>
2020-10-15 21:24:38 +0200

    Update

--- code/scripts/train_endd.py
+++ code/scripts/train_endd.py
@@ -3,16 +3,14 @@ Creates and trains a priornet on the EnDD data.
 '''
 import sys
 import os
-# parent_dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-# sys.path.append(parent_dir_path)
-sys.path.append('/home/lennelov/Repositories/endd-reproduce/code')
-import tensorflow as tf
+parent_dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(parent_dir_path)
 
+import tensorflow as tf
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 # Imports
-import tensorflow as tf
 import numpy as np
 import tensorflow.keras as keras
 import settings
@@ -23,7 +21,7 @@ from utils import evaluation, preprocessing, saveload, simplex, datasets
 # Set names for loading and saving
 ENSEMBLE_LOAD_NAME = 'vgg'  # Name of ensemble to use for training
 DATASET_NAME = 'cifar10'  # Name of dataset to use (ensemble must be trained on this dataset)
-MODEL_SAVE_NAME = 'endd_vgg'  # Name to use when saving model
+MODEL_SAVE_NAME = 'endd_vgg_cifar10'  # Name to use when saving model
 
 # Set training parameters
 N_MODELS = 30  # Number of models to include in ensemble
@@ -66,23 +64,24 @@ endd_model = endd.get_model(base_model)
 # Train ENDD model
 endd_model.fit(train_images, train_ensemble_preds, batch_size=BATCH_SIZE, epochs=N_EPOCHS)
 if MODEL_SAVE_NAME:
+    # Note: There seems to be some difficulties when trying to load whole model with custom loss
     saveload.save_tf_model(endd_model, MODEL_SAVE_NAME)
+    saveload.save_weights(endd_model, MODEL_SAVE_NAME)
 
-# Predict and show outputs
-logits = endd_model.predict(test_images)
-alphas = tf.math.exp(logits)
-predictions = tf.cast(tf.math.argmax(tf.squeeze(logits), axis=1), dtype=tf.float32)
-test_labels_tf = tf.cast(tf.squeeze(test_labels.copy()), dtype=tf.float32)
-
-score = tf.math.reduce_sum(tf.cast(tf.math.equal(predictions, test_labels_tf),
-                                   tf.float32)) / len(test_labels_tf)
-print('alphas for picture 1: ' + str(alphas[0, :]))
-print('alphas for picture 1: ' + str(alphas[1, :]))
-print('alphas for picture 1: ' + str(alphas[2, :]))
-print('mean of 5 ensembles for picture 1: ' +
-      str(tf.math.reduce_mean(tf.nn.softmax(train_ensemble_preds[0, :, :], axis=1), axis=0)))
-print('score: ' + str(score))
-
+# # Predict and show outputs
+# logits = endd_model.predict(test_images)
+# alphas = tf.math.exp(logits)
+# predictions = tf.cast(tf.math.argmax(tf.squeeze(logits), axis=1), dtype=tf.float32)
+# test_labels_tf = tf.cast(tf.squeeze(test_labels.copy()), dtype=tf.float32)
+#
+# score = tf.math.reduce_sum(tf.cast(tf.math.equal(predictions, test_labels_tf),
+#                                    tf.float32)) / len(test_labels_tf)
+# print('alphas for picture 1: ' + str(alphas[0, :]))
+# print('alphas for picture 1: ' + str(alphas[1, :]))
+# print('alphas for picture 1: ' + str(alphas[2, :]))
+# print('mean of 5 ensembles for picture 1: ' +
+#       str(tf.math.reduce_mean(tf.nn.softmax(train_ensemble_preds[0, :, :], axis=1), axis=0)))
+# print('score: ' + str(score))
 
 # Evaluate
 measures = evaluation.calc_classification_measures(endd_model,
