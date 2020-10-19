92cbbc97915092cd308d059c99e46dad69543b03
lennelov <lennelov@kth.se>
2020-10-19 19:30:30 +0200

    Remove unused import

--- code/utils/measures.py
+++ code/utils/measures.py
@@ -1,6 +1,5 @@
 """Module with functions to compute some measures."""
 import tensorflow as tf
-import tensorflow_probability as tfp
 import numpy as np
 import sklearn.metrics
 from uncertainty_metrics.numpy.general_calibration_error import ece
