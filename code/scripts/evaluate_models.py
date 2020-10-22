4e6dc559dab6ecd0c6868946e0c89109610a69b1
AronNoren <norenaron@gmail.com>
2020-10-22 15:53:31 +0200

    added PN+AUX to model evaluation

--- code/scripts/evaluate_models.py
+++ code/scripts/evaluate_models.py
@@ -11,14 +11,14 @@ physical_devices = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 from utils import evaluation, datasets, saveload, preprocessing
-from models import ensemble, endd
+from models import ensemble, endd, cnn_priorNet
 
 # Choose models
 IND_MODEL_NAME = 'vgg_cifar10_cifar10_0'
 ENSM_MODEL_NAME, ENSM_N_MODELS = 'vgg', 30
 ENDD_MODEL_NAME, ENDD_BASE_MODEL = 'endd_vgg_cifar10_extended', 'vgg'
 ENDD_AUX_MODEL_NAME, ENDD_AUX_BASE_MODEL = 'endd_vgg_cifar10_aux', 'vgg'
-
+PN_AUX_MODEL_NAME, PN_AUX_BASE_MODEL = 'PN_vgg_cifar10_aux','vgg'
 # Choose dataset
 DATASET_NAME = 'cifar10'
 
@@ -50,6 +50,13 @@ endd_aux_model = endd.get_model(ENDD_AUX_BASE_MODEL,
                                 weights=ENDD_AUX_MODEL_NAME)
 endd_aux_wrapper_type = 'individual'
 
+# Prepare PN+AUX model
+pn_aux_model = cnn_priorNet.get_model(PN_AUX_BASE_MODEL,
+                                      dataset_name = DATASET_NAME,
+                                      compile = True,
+                                      weights = PN_AUX_MODEL_NAME)
+pn_aux_wrapper_type = 'individual'
+
 # Load data
 _, (test_images, test_labels) = datasets.get_dataset(DATASET_NAME)
 
@@ -77,11 +84,16 @@ endd_aux_measures = evaluation.calc_classification_measures(endd_aux_model,
                                                             test_images,
                                                             test_labels,
                                                             wrapper_type=endd_aux_wrapper_type)
+print("Evaluating PN+AUX...")
+pn_aux_measures = evaluation.calc_classification_measures(pn_aux_model,
+                                                            test_images,
+                                                            test_labels,
+                                                            wrapper_type=pn_aux_wrapper_type)
 print("Evaluations complete.")
 
 # Format and print results
-summary = evaluation.format_results(['IND', 'ENSM', 'ENDD', 'ENDD+AUX'],
-                                    [ind_measures, ensm_measures, endd_measures, endd_aux_measures],
+summary = evaluation.format_results(['IND', 'ENSM', 'ENDD', 'ENDD+AUX', 'PN_AUX'],
+                                    [ind_measures, ensm_measures, endd_measures, endd_aux_measures,pn_aux_measures],
                                     dataset_name=DATASET_NAME)
 
 print(summary)
