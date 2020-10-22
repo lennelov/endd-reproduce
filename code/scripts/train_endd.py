84f143a2c3018ec8d4c5728bbd4ac6907e2895be
Einar Lennelöv <39123876+lennelov@users.noreply.github.com>
2020-10-22 13:38:45 +0200

    Add ensemble size ablation study (#53)
    Also does some refactoring

--- code/scripts/train_endd.py
+++ code/scripts/train_endd.py
@@ -17,8 +17,8 @@ import numpy as np
 import tensorflow.keras as keras
 import settings
 
-from models import vgg, cnn, endd, ensemble
-from utils import evaluation, preprocessing, saveload, simplex, datasets, callbacks
+from models import endd, ensemble
+from utils import saveload, training, evaluation, datasets, preprocessing
 
 # Set names for loading and saving
 ENSEMBLE_LOAD_NAME = 'vgg'  # Name of ensemble to use for training
@@ -27,16 +27,16 @@ AUX_DATASET_NAME = 'cifar100'  # Name of auxiliary dataset to use (None if no AU
 MODEL_SAVE_NAME = 'endd_vgg_cifar10_aux'  # Name to use when saving model (None if no saving)
 
 # Set training parameters
-N_MODELS = 30  # Number of models to include in ensemble
+N_MODELS = 30  # Number of models to include in ensemble, set to None if all should be included
 N_EPOCHS = 90  # Number of epochs to train for (90)
-BATCH_SIZE = 128  # Batch size
-NORMALIZATION = "-1to1"  # Normalization scheme to use {'-1to1', 'gaussian', None}
+BATCH_SIZE = 128  # Batch size (128)
+NORMALIZATION = "-1to1"  # Normalization scheme to use {'-1to1', 'gaussian', None} ('-1to1')
 # WARNING: It is important that normalization matches the normalization used when training
 #          the ensemble models.
 
 TEMP_ANNEALING = True
 ONE_CYCLE_LR_POLICY = True
-CYCLE_LENGTH = 60  # (90)
+CYCLE_LENGTH = 60  # (60)
 INIT_LR = 0.001  # (0.001)
 DROPOUT_RATE = 0.3  # (0.3)
 INIT_TEMP = 10  # (10)
@@ -44,7 +44,8 @@ INIT_TEMP = 10  # (10)
 # Load ensemble models
 ensemble_model_names = saveload.get_ensemble_model_names()
 model_names = ensemble_model_names[ENSEMBLE_LOAD_NAME][DATASET_NAME]
-model_names = model_names[:N_MODELS]
+if N_MODELS is not None:
+    model_names = model_names[:N_MODELS]
 wrapped_models = [ensemble.KerasLoadsWhole(name) for name in model_names]
 
 # Build ensemble
@@ -65,62 +66,29 @@ elif NORMALIZATION == 'gaussian':
     train_images, mean, std = preprocessing.normalize_gaussian(train_images)
     test_images = preprocessing.normalize_gaussian(test_images, mean, std)
 
-# Get ensemble preds
-train_ensemble_preds = datasets.get_ensemble_preds(ensemble_model, train_images)
-test_ensemble_preds = datasets.get_ensemble_preds(ensemble_model, test_images)
+endd_model = training.train_vgg_endd(train_images=train_images,
+                                     ensemble_model=ensemble_model,
+                                     dataset_name=DATASET_NAME,
+                                     batch_size=BATCH_SIZE,
+                                     n_epochs=N_EPOCHS,
+                                     one_cycle_lr_policy=ONE_CYCLE_LR_POLICY,
+                                     init_lr=INIT_LR,
+                                     cycle_length=CYCLE_LENGTH,
+                                     temp_annealing=TEMP_ANNEALING,
+                                     init_temp=INIT_TEMP,
+                                     dropout_rate=DROPOUT_RATE,
+                                     save_endd_dataset=True,
+                                     load_previous_endd_dataset=False)
 
-# Save / Load pickled data. Generating ensemble preds takes a long time, so saving and
-# loading can make testing much more efficient.
-with open('train.pkl', 'wb') as file:
-    pickle.dump((train_images, train_labels, train_ensemble_preds), file)
-with open('test.pkl', 'wb') as file:
-    pickle.dump((test_images, test_labels, test_ensemble_preds), file)
-
-# with open('train.pkl', 'rb') as file:
-#     train_images, train_labels, train_ensemble_preds = pickle.load(file)
-# with open('test.pkl', 'rb') as file:
-#     test_images, test_labels, test_ensemble_preds = pickle.load(file)
-
-# Image augmentation
-data_generator = preprocessing.make_augmented_generator(train_images, train_ensemble_preds,
-                                                        BATCH_SIZE)
-
-# Callbacks
-endd_callbacks = []
-if ONE_CYCLE_LR_POLICY:
-    olp_callback = callbacks.OneCycleLRPolicy(init_lr=INIT_LR,
-                                              max_lr=INIT_LR * 10,
-                                              min_lr=INIT_LR / 1000,
-                                              cycle_length=CYCLE_LENGTH,
-                                              epochs=N_EPOCHS)
-    endd_callbacks.append(olp_callback)
-
-if TEMP_ANNEALING:
-    temp_callback = callbacks.TemperatureAnnealing(init_temp=INIT_TEMP,
-                                                   cycle_length=CYCLE_LENGTH,
-                                                   epochs=N_EPOCHS)
-    endd_callbacks.append(temp_callback)
-
-if not endd_callbacks:
-    endd_callbacks = None
-
-# Build ENDD model
-base_model = vgg.get_model(DATASET_NAME, compile=False, dropout_rate=DROPOUT_RATE)
-endd_model = endd.get_model(base_model, init_temp=INIT_TEMP, teacher_epsilon=1e-4)
-
-# Train ENDD model
-endd_model.fit(data_generator, epochs=N_EPOCHS, callbacks=endd_callbacks)
+measures = evaluation.calc_classification_measures(endd_model,
+                                                   test_images,
+                                                   test_labels,
+                                                   wrapper_type='individual')
 
 if MODEL_SAVE_NAME:
     # Note: There seems to be some difficulties when trying to load whole model with custom loss
     saveload.save_tf_model(endd_model, MODEL_SAVE_NAME)
     saveload.save_weights(endd_model, MODEL_SAVE_NAME)
-
-# Evaluate
-measures = evaluation.calc_classification_measures(endd_model,
-                                                   test_images,
-                                                   test_labels,
-                                                   wrapper_type='individual')
 
 results = evaluation.format_results(['endd'], [measures])
 print(results)
