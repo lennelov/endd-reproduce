b69a932745d3a81b9c294e62d26928360338fd62
AronNoren <norenaron@gmail.com>
2020-10-24 17:16:30 +0200

    script to compare 3class predictions in simplex plot

---
+++ code/scripts/plot_simplex.py
@@ -0,0 +1,30 @@
+'''
+Plots a simplex for each of the models on cifar10 data with three classes ID and seven classes OOD. 
+'''
+import sys
+import pathlib
+import os
+parent_dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(parent_dir_path)
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras import datasets
+
+import settings
+
+from utils import preprocessing, saveload, simplex, losses, training
+from models import cnn_priorNet
+
+(_,_), (test_images, _) = datasets.cifar10.load_data()
+
+test_image = test_images[0,:,:,:]
+pn_model = saveload.load_tf_model("PN_vgg_cifar_3_class")
+ensemble_model = saveload.load_tf_model("ensemble_vgg_cifar_3_class")
+endd_model = saveload.load_tf_model("EnDD_vgg_cifar_3_class")
+
+
+pn_alphas = tf.math.exp(pn_model.predict(test_image))
+ensemble_alphas = tf.math.exp(ensemble_model.predict(test_image))
+endd_alphas = tf.math.exp(endd_model.predict(test_image))
+
+simplex.plot_simplex(alphas)
