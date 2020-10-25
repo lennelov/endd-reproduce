9255bc9c69064b0aafd796b550595b9c396abe5c
AronNoren <norenaron@gmail.com>
2020-10-25 14:23:13 +0100

    making PN model accept vgg architecture

--- code/models/cnn_priorNet.py
+++ code/models/cnn_priorNet.py
@@ -1,51 +1,36 @@
-import tensorflow as tf
-from tensorflow.keras import layers, models
-import sys
-import pathlib
-import os
-parent_dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-sys.path.append(parent_dir_path)
-import settings
-from utils import losses
+from utils import losses, saveload
+from models import cnn, vgg
+import tensorflow.keras as keras
 
 
-def get_model(dataset_name, compile=True, weights=None):
-    """Take dataset name and return corresponding untrained CNN model.
-
-	    Args:
-		dataset_name (str): Name of the dataset that the model will be used on,
-		                    must be listed in settings.py.
-		compile (bool): If False, an uncompiled model is returned. Default is True.
-		weights (str): Name of saved weights. If provided, returned model will
-		               be loaded with saved weights. Default is None.
-	    Returns:
-		keras Model object
-	    If compile=True, model will be compiled with adam optimizer, categorical cross
-	    entropy loss, and accuracy metric.
-	"""
-
-    if dataset_name not in settings.DATASET_NAMES:
-        raise ValueError("""Dataset {} not recognized, please make sure it has been listed in
-                            settings.py""".format(dataset_name))
-
-    input_shape = settings.DATASET_INPUT_SHAPES[dataset_name]
-    model = models.Sequential()
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-    model.add(layers.Flatten())
-    model.add(layers.Dense(64, activation='relu'))
-    model.add(layers.Dense(settings.DATASET_N_CLASSES[dataset_name], activation='exponential'))
+def get_model(base_model,
+              dataset_name=False,
+              compile=True,
+              weights=None,
+              dropout_rate=0.3,
+              epsilon = 1e-8,
+              softmax = False):
+    """Take an uncompiled model and return model compiled for PN.
+    Warning: This function works in place. Model is returned only for
+    conveniance.
+    """
+    if isinstance(base_model, str):
+        if not dataset_name:
+            raise ValueError('dataset_name must be provided if base_model is given by name.')
+        if base_model == 'cnn':
+            base_model = cnn.get_model(dataset_name, compile=False, softmax=False)
+        elif base_model == 'vgg':
+            base_model = vgg.get_model(dataset_name, compile=False, softmax=False,dropout_rate=dropout_rate)
+        else:
+            raise ValueError("""Base model {} not recognized, make sure it has been added
+                              to endd.py, or pass a Keras model object as base model instead.""")
 
     if weights:
-        saveload.load_weights(model, weights)
+        saveload.load_weights(base_model, weights)
 
     if not compile:
-        return model
+        return base_model
 
-    KL = losses.DirichletKL()
-    model.compile(optimizer='adam', loss=KL)
+    base_model.compile(optimizer='adam', loss=losses.DirichletKL())
 
-    return model
+    return base_model
