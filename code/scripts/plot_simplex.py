8943e4e19c6a3188fb8851e843cecf9386ebd556
lennelov <lennelov@kth.se>
2021-01-13 18:14:01 +0100

    Update

--- code/scripts/plot_simplex.py
+++ code/scripts/plot_simplex.py
@@ -17,53 +17,17 @@ from utils import datasets
 import pickle
 import matplotlib.pyplot as plt
 
+# ======== PARAMETERS ========
+
 DATASET_NAME = 'cifar10'
+THREE_CLASS_DATASET_NAME = 'cifar10_3_class'
+ENSM_MODEL_NAME, ENSM_N_MODELS = 'vgg_3_class', 20
+ENDD_MODEL_NAME  = "endd_vgg_cifar10_3class_aux"
+LOAD_PREVIOUS_ENSM_PREDS = False
+LOAD_PREVIOUS_ENDD_PREDS = False
 
-# # ======== SETUP =========
 
-# Load test images
-(train_images, train_labels), (test_images, test_labels) = datasets.get_dataset(DATASET_NAME)
-raw_test_images = test_images
-test_images = preprocessing.normalize_minus_one_to_one(test_images, min=0, max=255)
-#
-# Load ensemble
-ENSM_MODEL_NAME, ENSM_N_MODELS = 'vgg_3_class', 100
-DATASET_NAME = 'cifar10_3_class'
-ensemble_model_names = saveload.get_ensemble_model_names()
-model_names = ensemble_model_names[ENSM_MODEL_NAME][DATASET_NAME][:ENSM_N_MODELS]
-models = [ensemble.KerasLoadsWhole(name, pop_last=True) for name in model_names]
-ensm_model = ensemble.Ensemble(models)
-
-# Predict ensemble
-ensm_out = ensm_model.predict(test_images)
-
-# Save ensemble
-with open("ensemble_out.pkl", 'wb') as file:
-    pickle.dump((ensm_out), file)
-
-# Load endd
-endd_model = saveload.load_tf_model("endd_vgg_cifar10_3class_aux", compile=False)
-endd_model = endd.get_model(endd_model, init_temp=1, teacher_epsilon=1e-4)
-
-# Predict endd
-endd_out = endd_model.predict(test_images)
-
-# Save endd
-with open("endd_out.pkl", 'wb') as file:
-    pickle.dump((endd_out), file)
-
-noise_img = np.random.randn(1, 32, 32, 3)
-noise_ens = ensm_model.predict(noise_img)
-noise_endd = endd_model.predict(noise_img)
-
-# ======== MAIN SCRIPT =========
-
-with open("ensemble_out.pkl", 'rb') as file:
-    ens_out = pickle.load(file)
-
-with open("endd_out.pkl", 'rb') as file:
-    endd_out = pickle.load(file)
-
+# ======== FUNCTIONS =========
 
 def prepare_prediction(x):
     x = np.exp(np.float64(x))
@@ -71,14 +35,16 @@ def prepare_prediction(x):
     return x / x_2
 
 
-def compare_simplex(data_logits, know_logits, certain_logits, d, e, f, ens_noise, endd_noise, imgs_in, filename = None):
-    alphas = [prepare_prediction(data_logits),
-              prepare_prediction(know_logits),
-              prepare_prediction(certain_logits),
-              prepare_prediction(ens_noise)]
-    exped = [np.float64(d),
-            np.float64(e),
-            np.float64(f),
+def compare_simplex(ensm_data_uncertain, ensm_know_uncertain, ensm_certain, ensm_noise,
+                    endd_data_uncertain, endd_know_uncertain, endd_certain, endd_noise,
+                    imgs_in, filename = None):
+    alphas = [prepare_prediction(ensm_data_uncertain),
+              prepare_prediction(ensm_know_uncertain),
+              prepare_prediction(ensm_certain),
+              prepare_prediction(ensm_noise)]
+    exped = [np.float64(endd_data_uncertain),
+            np.float64(endd_know_uncertain),
+            np.float64(endd_certain),
             np.float64(endd_noise)]
 
     font = {
@@ -103,15 +69,11 @@ def compare_simplex(data_logits, know_logits, certain_logits, d, e, f, ens_noise
         plot_alphas = alphas[i]
         plot_logits = np.exp(np.array(exped[i]))
         plot_logits[plot_logits > 100] = 100
-        # print(plot_logits)
 
         simplex.plot_points(plot_alphas, alpha=0.3)
 
         plt.subplot(3, 4, i + 9)
 
-        # print(plot_logits.shape)
-        # print(np.sum(plot_logits))
-        # print(np.multiply.reduce([gamma(a) for a in plot_logits]))
         try:
             simplex.draw_pdf_contours(simplex.Dirichlet(np.float64(plot_logits)), nlevels=200, subdiv=3)
         except:
@@ -124,64 +86,103 @@ def compare_simplex(data_logits, know_logits, certain_logits, d, e, f, ens_noise
     plt.show()
 
 
+# ======== PREPARE IMAGES =========
 
+# Load test images
+(train_images, train_labels), (test_images, test_labels) = datasets.get_dataset(DATASET_NAME)
+raw_test_images = test_images
+test_images = preprocessing.normalize_minus_one_to_one(test_images, min=0, max=255)
+noise_img = np.random.randn(1, 32, 32, 3)
 
+# ======== PREDICTIONS =========
 
-# img_1 = 112
-# img_2 = 113  # 2
-# img_3 = 114  # 3
-#
-# # Pick out selected images
-# a = ens_out[:, img_1, :]
-# b = ens_out[:, img_2, :]
-# c = ens_out[:, img_3, :]
-# d = endd_out[img_1, :]
-# e = endd_out[img_2, :]
-# f = endd_out[img_3, :]
+if LOAD_PREVIOUS_ENSM_PREDS:
+    with open("ensm_preds.pkl", 'rb') as file:
+        ensm_preds = pickle.load(file)
+    with open("ensm_preds_noise.pkl", 'rb') as file:
+        ensm_preds_noise = pickle.load(file)
+else:
+    # Load ensemble
+    ensemble_model_names = saveload.get_ensemble_model_names()
+    model_names = ensemble_model_names[ENSM_MODEL_NAME][THREE_CLASS_DATASET_NAME][:ENSM_N_MODELS]
+    models = [ensemble.KerasLoadsWhole(name, pop_last=True) for name in model_names]
+    ensm_model = ensemble.Ensemble(models)
 
-# Pick out specific class
-indx = test_labels == 0
-indx = indx.flatten()
-imgs_plane = raw_test_images[indx]
-ens_plane = ens_out[:, indx, :]
-endd_plane = endd_out[indx, :]
+    # Predict ensemble
+    ensm_preds = ensm_model.predict(test_images)
+    with open("ensm_preds.pkl", 'wb') as file:
+        pickle.dump((ensm_preds), file)
+    ensm_preds_noise = ensm_model.predict(test_images)
+    with open("ensm_preds_noise.pkl", 'wb') as file:
+        pickle.dump((ensm_preds_noise), file)
 
-indx = test_labels == 4
-indx = indx.flatten()
-imgs_deer = raw_test_images[indx]
-ens_deer = ens_out[:, indx, :]
-endd_deer = endd_out[indx, :]
+if LOAD_PREVIOUS_ENDD_PREDS:
+    with open("endd_preds.pkl", 'rb') as file:
+        endd_preds = pickle.load(file)
+    with open("endd_preds_noise.pkl", 'rb') as file:
+        endd_preds_noise = pickle.load(file)
+else:
+    # Load endd
+    endd_base_model = saveload.load_tf_model(ENDD_MODEL_NAME, compile=False)
+    endd_model = endd.get_model(endd_base_model, init_temp=1, teacher_epsilon=1e-4)
 
-deer_unct_tot = measures.entropy_of_expected(ens_deer, True)
-deer_unct_data = measures.expected_entropy(ens_deer, True)
-deer_unct_know = deer_unct_tot - deer_unct_data
-print("Five most certain deer: {}".format(np.argsort(deer_unct_tot)[:5]))  # Five most certain
-print("Five most data uncertain deer: {}".format(np.argsort(deer_unct_data)[-5:]))  # Five most data uncertain
-print("Five most knowledge uncertain deer: {}".format(np.argsort(deer_unct_know)[-5:]))  # Five most knowledge uncertain
+    # Predict endd
+    endd_preds = endd_model.predict(test_images)
+    with open("endd_preds.pkl", 'wb') as file:
+        pickle.dump((endd_preds), file)
+    endd_preds_noise = ensm_model.predict(noise_img)
+    with open("endd_preds_noise.pkl", 'wb') as file:
+        pickle.dump((endd_preds_noise), file)
 
-plane_unct_tot = measures.entropy_of_expected(ens_plane, True)
-plane_unct_data = measures.expected_entropy(ens_plane, True)
-plane_unct_know = plane_unct_tot - plane_unct_data
-print("Five most certain plain: {}".format(np.argsort(plane_unct_tot)[:5]))  # Five most certain
-print("Five most data uncertain plane: {}".format(np.argsort(plane_unct_data)[-5:]))  # Five most data uncertain
-print("Five most knowledge uncertain plane: {}".format(np.argsort(plane_unct_know)[-5:]))  # Five most knowledge uncertain
+# ===== SELECT EXAMPLES =====
 
+# Pick out plane images and preds
+indx_plane = test_labels == 0
+indx_plane = indx_plane.flatten()
+imgs_plane = raw_test_images[indx_plane]
+ensm_preds_plane = ensm_preds[:, indx_plane, :]
+endd_preds_plane = endd_preds[indx_plane, :]
+
+# Pick out deer images and preds
+for label in test_labels:
+    print(label)
+print(test_labels)
+indx_deer = test_labels == 4
+indx_deer = indx_deer.flatten()
+print(indx_deer)
+imgs_deer = raw_test_images[indx_deer]
+ensm_preds_deer = ensm_preds[:, indx_deer, :]
+endd_preds_deer = endd_preds[indx_deer, :]
+
+unct_tot_deer = measures.entropy_of_expected(ensm_preds_deer, True)
+unct_data_deer = measures.expected_entropy(ensm_preds_deer, True)
+unct_know_deer = unct_tot_deer - unct_data_deer
+print("Five most certain deer: {}".format(np.argsort(unct_tot_deer)[:5]))  # Five most certain
+print("Five most data uncertain deer: {}".format(np.argsort(unct_data_deer)[-5:]))  # Five most data uncertain
+print("Five most knowledge uncertain deer: {}".format(np.argsort(unct_know_deer)[-5:]))  # Five most knowledge uncertain
+
+unct_tot_plane = measures.entropy_of_expected(ensm_preds_plane, True)
+unct_data_plane = measures.expected_entropy(ensm_preds_plane, True)
+unct_know_plane = unct_tot_plane - unct_data_plane
+print("Five most certain plain: {}".format(np.argsort(unct_tot_plane)[:5]))  # Five most certain
+print("Five most data uncertain plane: {}".format(np.argsort(unct_data_plane)[-5:]))  # Five most data uncertain
+print("Five most knowledge uncertain plane: {}".format(np.argsort(unct_know_plane)[-5:]))  # Five most knowledge uncertain
+
+# Hard coded image indices chosen based on above printouts
 data_uncertain_deer = 799
 knowledge_uncertain_plane = 653
-certain_deer = 682  # 3
+certain_deer = 682
 
-ens_class_1 = ens_deer[:, data_uncertain_deer, :]
-ens_class_2 = ens_plane[:, knowledge_uncertain_plane, :]
-ens_class_3 = ens_deer[:, certain_deer, :]
-ens_noise = noise_ens[:, 0, :]
-endd_class_1 = endd_deer[data_uncertain_deer, :]
-endd_class_2 = endd_plane[knowledge_uncertain_plane, :]
-endd_class_3 = endd_deer[certain_deer, :]
-endd_noise = noise_endd[0, :]
+ensm_data_uncertain = ensm_preds_deer[:, data_uncertain_deer, :]
+ensm_know_uncertain = ensm_preds_plane[:, knowledge_uncertain_plane, :]
+ensm_certain = ensm_preds_deer[:, certain_deer, :]
+ensm_noise = ensm_preds_noise[:, 0, :]
+endd_data_uncertain = endd_preds_deer[data_uncertain_deer, :]
+endd_know_uncertain = endd_preds_plane[knowledge_uncertain_plane, :]
+endd_certain = endd_preds_deer[certain_deer, :]
+endd_noise = endd_preds_noise[0, :]
 
 imgs_in = [imgs_deer[data_uncertain_deer], imgs_plane[knowledge_uncertain_plane], imgs_deer[certain_deer], noise_img[0]]
-# ens_outs
-# endd_outs =
-compare_simplex(ens_class_1, ens_class_2, ens_class_3, endd_class_1, endd_class_2, endd_class_3, ens_noise, endd_noise, imgs_in)
-
-#simplex.plot_points(a, barycentric = True, border = True)
+compare_simplex(ensm_data_uncertain, ensm_know_uncertain, ensm_certain, ensm_noise,
+                endd_data_uncertain, endd_know_uncertain, endd_certain, endd_noise,
+                imgs_in)
