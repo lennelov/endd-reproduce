d61d298b52c4338e07d7cd4a3fdc65f1de1bcbf1
lennelov <lennelov@kth.se>
2021-01-30 01:54:16 +0100

    Anonymize

--- code/scripts/plot_simplex.py
+++ code/scripts/plot_simplex.py
@@ -1,8 +1,7 @@
 import sys
 import pathlib
 import os
-#parent_dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-sys.path.append("/home/lennelov/Repositories/endd-reproduce/code")
+parent_dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 import tensorflow as tf
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
